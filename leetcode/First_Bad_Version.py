97b76759b5f1b60da59f19a649a52ca91a9f4dc9
Prateek Gupta <prateekg.9501@gmail.com>
2020-05-16 16:49:53 +0530

    more leetcode problems

---
+++ leetcode/First_Bad_Version.py
@@ -0,0 +1,31 @@
+# Example:
+#
+# Given n = 5, and version = 4 is the first bad version.
+#
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+#
+# Then 4 is the first bad version.
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 1
+        end = n
+        while start < end:
+            mid=start+(end-start)//2
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid + 1
+        return start
